64ec35d3f1e9f1938754ccf649debe980cf2a0dd
coltondane <113216904+coltondane@users.noreply.github.com>
2023-10-03 20:46:50 -0600

    working on API fetch

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,21 @@
 const router = require('express').Router();
 const { User } = require('../models');
 const apiKey = '3db7fef6c0msh02dc16734cc3a68p1497c9jsn2626d897910e';
+
 // const loggedIn = require('../utils/'); --> imports authentication function once created 
 
+// functions
+function getPlanetData(selectedPlanet) {
+    var apiUrl = 'https://api.api-ninjas.com/v1/planets?name=' + planetName;
 
-
-
+    fetch(apiUrl)
+    .then(function (response) {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Error");
+        }
+      });
+}
 
 module.exports = router;
