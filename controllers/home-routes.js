bbe1299528be9136bb859d5b66007ddaede69f1c
coltondane <113216904+coltondane@users.noreply.github.com>
2023-10-03 20:31:18 -0600

    implementing home routes, added API key

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const { User } = require('../models');
+const apiKey = '3db7fef6c0msh02dc16734cc3a68p1497c9jsn2626d897910e';
 // const loggedIn = require('../utils/'); --> imports authentication function once created 
+
+
 
 
 
