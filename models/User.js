e7800811ae20ef55b7449f1173dea82a83500e7f
Jessie Shockey <shockeyjessie92@gmail.com>
2023-10-05 18:19:36 -0600

    Create User Model

--- models/User.js
+++ models/User.js
@@ -9,14 +9,41 @@ class User extends Model {
 }
 
 User.init(
-    { /**TODO: TABLE COLUMNS GET DEFINED HERE */   },
-    {
-      sequelize,
-      timestamps: false,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'user',
-    }
+    {     
+      id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        validatePassword: function(password) {
+          if(!(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,12}$/.test(password))) {
+              throw new Error('The password must contain at least 8 and maximum 12 characters including at least 1 uppercase, 1 lowercase, one number and one special character.');
+          }
+        }
+      },   
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
   );
 
 
