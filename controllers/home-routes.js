c131bdeee89f68d41e8c464240277122263baf08
coltondane <113216904+coltondane@users.noreply.github.com>
2023-10-05 19:32:43 -0600

    finished main data homepage route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,32 +2,32 @@ const router = require('express').Router();
 const { User, Mission, Planet, Log } = require('../models');
 const withAuth = require('./utils/auth');
 
-// Authorization middleware
-
-// get all missions
-// get total number of misions
+// GET homepage data for the user
 router.get('/', withAuth, async (req, res) => {
     try {
         const missionData = await Mission.findAll({
             include: [
                 {
                     model: User,
-                    where: { id: req.session.user_id}
-                },
-                {
-                    model: Mission,
-                    where: { id: req.session.user_id}
+                    where: { id: req.session.user_id }
                 },
                 {
                     model: Planet,
-                    where: { id: req.session.user_id}
                 },
                 {
                     model: Log,
-                    where: { id: req.session.user_id}
                 }
             ]
-        })
+        });
+
+        // Serialize creating ana rray of mission objects
+        const missions = missionData.map((mission) => mission.get({ plain: true }));
+
+        // render
+        res.render('homepage', {
+            missions,
+            logged_in: req.session.logged_in
+        });
     } catch (error) {
         res.status(500).json(error);
     }
