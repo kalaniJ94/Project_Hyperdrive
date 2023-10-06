51430f0612a616694c3b6adf27c9c891acfea527
Jessie Shockey <shockeyjessie92@gmail.com>
2023-10-05 18:21:08 -0600

    Add hook to User Model

--- models/User.js
+++ models/User.js
@@ -38,13 +38,18 @@ User.init(
     },
   },
   {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
     sequelize,
-    timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'user',
   }
-  );
+);
 
 
 module.exports = User;
