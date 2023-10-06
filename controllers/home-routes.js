fa81cfa2645b47b83a01baf557ed6d3edbc760f5
coltondane <113216904+coltondane@users.noreply.github.com>
2023-10-05 19:21:13 -0600

    gathering model data

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Mission, Planet, Log } = require('../models');
 const withAuth = require('./utils/auth');
 
 // Authorization middleware
@@ -8,9 +8,28 @@ const withAuth = require('./utils/auth');
 // get total number of misions
 router.get('/', withAuth, async (req, res) => {
     try {
-        const missionData = await 
+        const missionData = await Mission.findAll({
+            include: [
+                {
+                    model: User,
+                    where: { id: req.session.user_id}
+                },
+                {
+                    model: Mission,
+                    where: { id: req.session.user_id}
+                },
+                {
+                    model: Planet,
+                    where: { id: req.session.user_id}
+                },
+                {
+                    model: Log,
+                    where: { id: req.session.user_id}
+                }
+            ]
+        })
     } catch (error) {
-        
+        res.status(500).json(error);
     }
 })
 
