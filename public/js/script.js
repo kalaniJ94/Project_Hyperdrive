9130b8671d7e70979015f0775d91f7cf99a484b5
coltondane <113216904+coltondane@users.noreply.github.com>
2023-10-10 21:19:11 -0600

    re-implementing random event function as a helper

--- public/js/script.js
+++ public/js/script.js
@@ -80,31 +80,6 @@ console.log(habitability);
 };
 getData();
 
-// random event function
-function randomSpaceEvent() {
-    const eventArray = [
-        'ALERT: Geomagnetic Storm Nearby, Avoid the Radiation', 
-        'ALERT: Sapce Debris Incoming!', 
-        'ALERT: Approaching Black Hole Event Horizon! Take Evasive Action!', 
-        'ALERT: Meteoroid Shower Closing In, Change Course!', 
-        'ALERT: You are Currently in a Supernova Kill Zone!',
-        'ALERT: Vacuum Leak Detected!', 
-        'ALERT: Mankind was born on Earth, it was never meant to die there...', 
-        'ALERT: SPACE WORMS', 
-        'ALERT: SPACE PIRATES', 
-        'ALERT: Unknown Impact! Investigate!', 
-        'ALERT: Flat Earther Detected Nearby, Avoid At All Costs!', 
-        'ALERT: Large Amounts of Radiation Detected', 
-        'ALERT: Wormhole Nearby']
-
-        // select random element
-        var event = eventArray[Math.floor(Math.random()*eventArray.length)];
-
-        return event;
-}
-randomSpaceEvent();
-
-
 //listen for clicks on the dropdown
 document.querySelector('.dropdown').addEventListener('click', function(event) {
     event.preventDefault();
