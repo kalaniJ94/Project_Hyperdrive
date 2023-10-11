30a3ad58a21a1cc687b29421a2def6d94e2186b4
coltondane <113216904+coltondane@users.noreply.github.com>
2023-10-10 21:07:47 -0600

    added random space event function

--- public/js/script.js
+++ public/js/script.js
@@ -80,6 +80,29 @@ console.log(habitability);
 };
 getData();
 
+// random event function
+function randomSpaceEvent() {
+    const eventArray = [
+        'ALERT: Geomagnetic Storm Nearby, Avoid the Radiation', 
+        'ALERT: Sapce Debris Incoming!', 
+        'ALERT: Approaching Black Hole Event Horizon! Take Evasive Action!', 
+        'ALERT: Meteoroid Shower Closing In, Change Course!', 
+        'ALERT: You are Currently in a Supernova Kill Zone!',
+        'ALERT: Vacuum Leak Detected!', 
+        'ALERT: Mankind was born on Earth, it was never meant to die there...', 
+        'ALERT: SPACE WORMS', 
+        'ALERT: SPACE PIRATES', 
+        'ALERT: Unknown Impact! Investigate!', 
+        'ALERT: Flat Earther Detected Nearby, Avoid At All Costs!', 
+        'ALERT: Large Amounts of Radiation Detected', 
+        'ALERT: Wormhole Nearby']
+
+        // select random element
+        var event = eventArray[Math.floor(Math.random()*eventArray.length)];
+
+        return event;
+}
+
 
 //listen for clicks on the dropdown
 document.querySelector('.dropdown').addEventListener('click', function(event) {
@@ -90,3 +113,4 @@ document.querySelector('.dropdown').addEventListener('click', function(event) {
         document.body.style.backgroundImage = `url('${selectedPlanet.image}')`;
       }
     });
+
