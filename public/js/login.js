3ffe0022981b4f9be31ac7462becdef44f897b94
Jessie Shockey <shockeyjessie92@gmail.com>
2023-10-10 20:50:14 -0600

    Create SignUp Functionality

--- public/js/login.js
+++ public/js/login.js
@@ -22,4 +22,29 @@ const loginFormHandler = async (event) => {
     }
   };
 
-document.querySelector('#loggin-btn').addEventListener('click', loginFormHandler)
+  const signUpFormHandler = async (event) => {
+    event.preventDefault();
+  
+    // Collect login values
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (email && password) {
+      // Send a POST request to the API endpoint
+      const response = await fetch('/api/users/', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        // If successful, redirect the browser to the profile page
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+
+document.querySelector('#loggin-btn').addEventListener('click', loginFormHandler);
+document.querySelector('#signup-btn').addEventListener('click', signUpFormHandler);
